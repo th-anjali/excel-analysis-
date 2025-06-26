bb5cda9179a800a3e995efa0e894b51afaa76740
th-anjali <anjaliyadav25102002@gmail.com>
2025-06-26 15:45:39 +0530

    Add new feature

---
+++ frontend/excel-analysis/src/Dashboard/Expense.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Expense = () => {
+    return (
+        <div>Expense</div>
+    )
+}
+export default Expense;
---
+++ frontend/excel-analysis/src/Dashboard/Income.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Income =() =>{
+    return (
+        <div>Income</div>
+    )
+}
+export default Income;
